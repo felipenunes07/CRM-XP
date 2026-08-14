7b3b0b524a17122ef354aa744862faf1eb94e464
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2026-08-14 10:29:14 -0300

    fix: support legacy smart tv browsers

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   envDir: "../..",
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["chrome >= 38", "safari >= 10", "ios >= 10", "samsung >= 4"],
+      modernTargets: ["chrome >= 61", "safari >= 11", "ios >= 11", "samsung >= 8"],
+      modernPolyfills: true,
+      additionalLegacyPolyfills: ["whatwg-fetch"],
+    }),
+  ],
   resolve: {
     extensions: [".ts", ".tsx", ".mjs", ".js", ".jsx", ".json"],
   },
